61d4665472659dc7f0e08aaea7364e6611d88886
Mike King <mike@85pixels.com>
2015-08-23 11:54:24 -1000

    Consolidate tests in to single document

--- test/app.js
+++ test/app.js
@@ -26,4 +26,24 @@ describe('microcebus:app', function(){
       'plugins'
     ]);
   });
+
+  it('adds the Foundation dependency', function(){
+    assert.fileContent('bower.json', 'foundation');
+  });
+
+  it('doesn\'t explicitly add the jQuery dependency', function(){
+    assert.noFileContent('bower.json', 'jquery');
+  });
+
+  it('adds Foundation paths to Gruntfile.js', function(){
+    assert.fileContent('Gruntfile.js', 'foundation');
+  });
+
+  it('adds the Grunt plugin', function(){
+    assert.fileContent('package.json', 'sass');
+  });
+
+  it('adds the Grunt task', function(){
+    assert.fileContent('Gruntfile.js', 'sass');
+  });
 });
