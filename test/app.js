bd9c62f228e131b4f9e57a3fd1b4d01a7afd0207
Mike King <mike@85pixels.com>
2015-08-23 12:06:38 -1000

    Updated test settings

--- test/app.js
+++ test/app.js
@@ -4,46 +4,52 @@ var path    = require('path');
 var assert  = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 
-describe('microcebus:app', function(){
+describe('microcebus', function(){
 
   before(function(done){
     helpers.run(path.join(__dirname, '../app'))
       .on('end', done);
   });
 
-  it('creates expected files', function(){
-    assert.file([
-      'bower.json',
-      '.bowerrc',
-      '.gitignore',
-      'Gruntfile.js',
-      'hologram_config.yml',
-      'package.json',
-      'README.md',
-      '.editorconfig',
-      'Gemfile',
-      '.jshintrc',
-      'plugins'
-    ]);
+  describe('app', function(){
+    it('creates expected files', function(){
+      assert.file([
+        'bower.json',
+        '.bowerrc',
+        '.gitignore',
+        'Gruntfile.js',
+        'hologram_config.yml',
+        'package.json',
+        'README.md',
+        '.editorconfig',
+        'Gemfile',
+        '.jshintrc',
+        'plugins'
+      ]);
+    });
   });
 
-  it('adds the Foundation dependency', function(){
-    assert.fileContent('bower.json', 'foundation');
+  describe('foundation', function(){
+    it('adds the Foundation dependency', function(){
+      assert.fileContent('bower.json', 'foundation');
+    });
+
+    it('doesn\'t explicitly add the jQuery dependency', function(){
+      assert.noFileContent('bower.json', 'jquery');
+    });
+
+    it('adds Foundation paths to Gruntfile.js', function(){
+      assert.fileContent('Gruntfile.js', 'foundation');
+    });
   });
 
-  it('doesn\'t explicitly add the jQuery dependency', function(){
-    assert.noFileContent('bower.json', 'jquery');
-  });
+  describe('sass', function(){
+    it('adds the Grunt plugin', function(){
+      assert.fileContent('package.json', 'sass');
+    });
 
-  it('adds Foundation paths to Gruntfile.js', function(){
-    assert.fileContent('Gruntfile.js', 'foundation');
-  });
-
-  it('adds the Grunt plugin', function(){
-    assert.fileContent('package.json', 'sass');
-  });
-
-  it('adds the Grunt task', function(){
-    assert.fileContent('Gruntfile.js', 'sass');
+    it('adds the Grunt task', function(){
+      assert.fileContent('Gruntfile.js', 'sass');
+    });
   });
 });
