9d8c1ad4831aded90487ea38727d1d2d4b02d574
Nainoa Shizuru <nainoa@ikayzo.com>
2016-01-13 12:42:49 -1000

    Updated grunt tasks

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -9,76 +9,149 @@ module.exports = function (grunt) {
       app: 'wp-content/themes/<%= appSlug %>',
       vendor: 'wp-content/themes/<%= appSlug %>/assets/vendor',
 
-      // CSS | Process SCSS files
+      /* 
+       * grunt-sass: Compile Sass to CSS using node-sass
+       * https://github.com/sindresorhus/grunt-sass
+       */
       sass: {
           options: {
               sourceMap: false,
               outputStyle: 'expanded',
-              includePaths: ['<%%= vendor %>/foundation/scss']
+              includePaths: [
+                '<%= vendor %>',
+                '<%= vendor %>/foundation/scss'
+              ]
           },
           dist: {
             files: [{
               expand: true,
-              cwd: '<%%= app %>/assets/scss/',
+              cwd: '<%= app %>/assets/scss/',
               src: ['{,*/}*.scss'],
-              dest: '<%%= app %>/',
+              dest: '<%= app %>/',
               ext: '.css'
             }]
           }
       },
 
-      // Add vendor prefixes
+      /* 
+       * PostCSS: CSS Transformer via JS plugins
+       * https://github.com/nDmitry/grunt-postcss
+       */
       postcss: {
-        options: {
-          processors: [
-            require('autoprefixer-core')({browsers: ['last 2 versions']})
-          ]
-        },
-        dist: {
+        serve: {
+          options: {
+            processors: [
+              /* 
+               * Autoprefixer: Add vendor prefixes using caniuse.com
+               * https://github.com/postcss/autoprefixer
+               */
+              require('autoprefixer')({
+                browsers: ['last 2 versions', 'ie >= 9']
+              })
+            ]
+          },
           files: [{
               expand: true,
-              cwd: '<%%= app %>/',
-              src: '{,*/}*.css',
-              dest: '<%%= app %>/'
+              cwd: '<%= app %>/',
+              src: '*.css',
+              dest: '<%= app %>/'
+          }]
+        },
+        dist: {
+          options: {
+            processors: [
+              require('autoprefixer')({
+                browsers: ['last 2 versions', 'ie >= 9']
+              }),
+              /* 
+               * CSS Nano: Modular minifier
+               * https://github.com/ben-eb/cssnano
+               */
+              require('cssnano')()
+            ]
+          },
+          files: [{
+              expand: true,
+              cwd: '<%= app %>/',
+              src: '*.css',
+              dest: '<%= app %>/'
           }]
         }
       },
 
-      // Minify CSS
-      cssmin: {
-        target: {
-          files: {
-            '<%%= app %>/style.css': '<%%= app %>/style.css'
-          }
-        }
-      },
-
-      // JS | Concat js files
-      concat: {
-        dist: {
-          src: ['<%%= vendor %>/jquery/dist/jquery.js',
-                '<%%= vendor %>/fastclick/lib/fastclick.js',
-                '<%%= vendor %>/foundation/js/foundation.js',
-                '<%%= app %>/assets/js/app.js'
-               ],
-          dest: '<%%= app %>/script.js',
-        },
-      },
-
-      // Check js files for errors
+      /*
+       * JSHint: Validate JavaScript files
+       * https://github.com/gruntjs/grunt-contrib-jshint
+       */
       jshint: {
           options: {
-            jshintrc: '.jshintrc'
+            node: true,
+            browser: true,
+            esnext: true,
+            bitwise: true,
+            camelcase: false,
+            curly: true,
+            eqeqeq: true,
+            immed: true,
+            indent: 2,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            quotmark: 'single',
+            regexp: true,
+            undef: true,
+            unused: true,
+            strict: true,
+            trailing: true,
+            smarttabs: true,
+            globals: {}
           },
           grunt: [
             'Gruntfile.js',
           ],
           scripts: [
-            '<%%= app %>/assets/js/app.js'
+            '<%= app %>/assets/js/app.js'
           ]
       },
 
-      // Minify js files
+      /*
+       * JSDoc: Generate JS documentation
+       * https://github.com/gruntjs/grunt-contrib-concat
+       */
+      jsdoc : {
+        options: {
+            destination: '<%= app %>/assets/docs/js'
+        },
+        dist : {
+            src: '<%= app %>/assets/js/**/*.js'
+        }
+      },
+
+      /*
+       * Concat: Concatenate JS files
+       * https://github.com/gruntjs/grunt-contrib-concat
+       */
+      concat: {
+        dist: {
+          src: [
+                // Libraries
+                '<%= vendor %>/jquery/dist/jquery.js',
+                '<%= vendor %>/fastclick/lib/fastclick.js',
+
+                  // include Foundation components separately
+                '<%= vendor %>/foundation/js/foundation.js',
+
+                // Custom functions
+                '<%= app %>/assets/js/app.js'
+               ],
+          dest: '<%= app %>/script.js',
+        },
+      },
+
+      /*
+       * Uglify: Minify JS files
+       * https://github.com/gruntjs/grunt-contrib-uglify
+       */
       uglify: {
         options: {
           preserveComments: false,
@@ -86,29 +159,32 @@ module.exports = function (grunt) {
         },
         dist: {
           files: {
-            '<%%= app %>/script.js': ['<%%= app %>/script.js']
+            '<%= app %>/script.js': ['<%= app %>/script.js']
           }
         }
       },
 
-      // Watch files for changes
+      /*
+       * Watch: Run tasks whenever watched files change
+       * https://github.com/gruntjs/grunt-contrib-watch
+       */
       watch: {
         options: {
           livereload: 35729
         },
         sass: {
-          files: '<%%= app %>/assets/scss/**/*.scss',
-          tasks: ['sass', 'postcss']
+          files: '<%= app %>/assets/scss/**/*.scss',
+          tasks: ['sass', 'postcss:serve']
         },
         php: {
-          files: ['<%%= app %>/**/*.php']
+          files: ['<%= app %>/**/*.php']
         },
         gruntfile: {
           files: 'Gruntfile.js',
           tasks: ['jshint:grunt']
         },
         scripts: {
-          files: '<%%= app %>/assets/js/app.js',
+          files: '<%= app %>/assets/js/app.js',
           tasks: ['jshint:scripts', 'concat']
         }
       }
@@ -118,20 +194,26 @@ module.exports = function (grunt) {
     // Development
     grunt.registerTask('serve', [
         'jshint',
-        'sass',
-        'postcss',
         'concat',
+        'sass',
+        'postcss:serve',
         'watch'
     ]);
 
     // Build
     grunt.registerTask('build', [
         'jshint',
-        'sass',
-        'postcss',
-        'cssmin',
+        'jsdoc',
         'concat',
-        'uglify'
+        'uglify',
+        'sass',
+        'postcss:dist',
+        'cssnano'
+    ]);
+
+    // Docs
+    grunt.registerTask('docs', [
+        'jsdoc',
     ]);
 
     // Default
