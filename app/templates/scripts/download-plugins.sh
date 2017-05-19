86201495b7e819c87b9df2ba76263414c0b37544
Mike King <yo@mikeking.io>
2017-05-18 15:44:23 -1000

    Resolving issue with plugin download hanging

--- app/templates/scripts/download-plugins.sh
+++ app/templates/scripts/download-plugins.sh
@@ -3,7 +3,7 @@ cat ./plugins | xargs -n 1 curl -O;
 echo 'Moving files...'
 mv *.zip ./wp-content/plugins;
 echo 'Unpacking plugin archive files..';
-yes | unzip ./wp-content/plugins/\*.zip -d ./wp-content/plugins/;
+find ./wp-content/plugins -depth -name '*.zip' -exec unzip {} -d ./wp-content/plugins/ \;
 echo 'Removing archives files...';
 rm -rf ./wp-content/plugins/*.zip;
 echo 'Done!';
